9f200e98d5dcfebfb7ef90039cd2a8da3c9a23a9
Elizabeth Dobbins <liz.dobbins4@gmail.com>
2025-09-05 11:18:27 -0800

    don't add duplicate records

--- src/water_level_play/defs/assets/water_level.py
+++ src/water_level_play/defs/assets/water_level.py
@@ -4,12 +4,13 @@ import dagster as dg
 import pandas as pd
 import requests
 from dagster_duckdb import DuckDBResource
+from duckdb import CatalogException
 
 
 @dg.asset
 def pt_townsand_live(database: DuckDBResource) -> None:
     """
-      Pulling real-time water level data for Port Townsand, WA. Loads into database.
+      Pulling real-time water level data for Port Townsand, WA. Loads into database. Cleans table every time.
     """
     url = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?'\
         'date=today&station=9444900&'\
@@ -35,8 +36,10 @@ def pt_townsand_clean(database: DuckDBResource) -> None:
       Appending a clean-up verion of the water level data into a database.
     """
 
+    # get the raw data and the last time collected so far
     with database.get_connection() as conn:
         pt_df = conn.execute("SELECT * FROM pt_townsand_raw").fetch_df()
+        last_time = conn.execute("select datetime from pt_townsand_clean order by datetime desc limit 1;").fetchone()[0]
 
     # cleanup
     pt_df['datetime'] = pd.to_datetime(pt_df['t'])
@@ -49,11 +52,18 @@ def pt_townsand_clean(database: DuckDBResource) -> None:
     })
     pt_df['value'] = pt_df['value'].astype(float)
 
-    # error flags
+    # cleanup error flags
     pt_df[['scatter', 'flat', 'rate_of_change', 'bounds']] = pt_df['data_flags'].str.split(',', expand=True).astype(int)
     pt_df['flag_summary'] = pt_df['scatter'] + pt_df['rate_of_change'] + pt_df['flat'] + pt_df['bounds']
 
-    query = "CREATE OR REPLACE TABLE pt_townsand_clean AS SELECT * FROM pt_df"
+    # limit new data to only times not collected yet
+    pt_df = pt_df.loc[pt_df['datetime'] > last_time]
 
+    query = "INSERT INTO pt_townsand_clean SELECT * FROM pt_df"
     with database.get_connection() as conn:
-        conn.execute(query)
+        try:
+            conn.execute(query)
+            # print("Appending cleaned data to an existing table.")
+        except CatalogException:
+            # print("Table does not exist. Creating table of cleaned data.")
+            conn.execute("CREATE OR REPLACE TABLE pt_townsand_clean AS SELECT * FROM pt_df")
